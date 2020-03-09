16e04342e7d7d1daf13b62dfd217d53b223aeaa8
ishaypaz31 <58163486+ishaypaz31@users.noreply.github.com>
2020-03-09 19:03:28 +0200

    Properties

---
+++ Examples/Properties/Fruit.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropExample
+{
+    class Fruit
+    {
+        private int _calories;
+
+
+        //Getter
+        public int GetCalories()
+        {
+            return _calories;
+        }
+
+        //Setter
+        internal void SetCalories(int calories)
+        {
+            if (calories > 0)
+            {
+                _calories = calories;
+            }
+        }
+
+        // Property
+        // 1 - access my data private
+
+        public int Calories
+        {
+            get
+            {
+                return _calories;
+            }
+            internal set // (int Value)
+            {
+                if (value > 0)
+                {
+                    _calories = value;
+                }
+            }
+
+        }
+
+        //2 create and access data
+        public string Name
+        {
+            get; //this create backing field
+            internal set;
+        }
+
+
+        //3 - Method like
+        public string Description
+        {
+            get
+            {
+                return $"{Name} + {Calories}";
+            }
+
+        }
+
+        //private string _name;
+        ////Getter
+        //public string GetName()
+        //{
+        //    return _name;
+        //}
+
+        ////Setter
+        //internal void SetName(string name)
+        //{
+        //    _name = name;
+
+        public Fruit(string name, int calories)
+        {
+            _calories = calories;
+             this.Name = name;
+            //_name = name;
+        }
+    }
+}
---
+++ Examples/Properties/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fruit fruit = new Fruit("Apple", 200);
+
+            int cal = fruit.GetCalories();
+
+            int cal2 = fruit.Calories;
+            fruit.Calories = 300;//change the value to 300 from 200
+
+            Console.WriteLine(fruit.Description);//apply Description function
+        }
+    }
+}
