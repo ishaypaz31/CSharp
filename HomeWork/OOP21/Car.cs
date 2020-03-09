be5d7393b60b3b0185dc4c784ad72ddbf713f527
ishaypaz31 <58163486+ishaypaz31@users.noreply.github.com>
2020-03-09 18:40:30 +0200

    Serialization

---
+++ HomeWork/OOP21/Car.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP21
+{
+    public class Car
+    {
+        public string Model { get; set; }
+
+        public string Brand { get; set; }
+
+        public int Year { get; set; }
+
+        public string Color { get;  set; }
+
+
+        private int codan = 0;
+
+        protected int numberOfSeats = 10;
+
+        public Car()
+        {
+
+        }
+
+        public Car(string model, string brand, int year, string color, int codan, int numberOfSeats)
+        {
+            Model = model;
+            Brand = brand;
+            Year = year;
+            Color = color;
+            this.codan = codan;
+            this.numberOfSeats = numberOfSeats;
+        }
+
+        public int GetCodan(int Coden)
+        {
+            return this.codan;
+        }
+
+        public int GetNumberOfSeats()
+        {
+            return this.numberOfSeats;
+        }
+
+        protected void cahngeNumberOfSeats(int newNumberOfSeats)
+        {
+            if(newNumberOfSeats > 0)
+            {
+                this.numberOfSeats = newNumberOfSeats;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Model: {Model} , Brand: {Brand} , Year: {Year} , Color: {Color}";
+        }
+    }
+}
---
+++ HomeWork/OOP21/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+namespace OOP21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Car c1 = new Car("BMW", "X8", 2020, "Dark Blue", 0, 4);
+
+            XmlSerializer xmlSerializerCar = new XmlSerializer(typeof(Car));
+
+            //using (Stream File = new FileStream(@"c:\XmlTest\xmlfile.xml", FileMode.Create)) //creating new file stream
+            //{
+            //    xmlSerializerCar.Serialize(File, c1);
+            //}//close stream
+
+            Car c2;
+
+            using (Stream file = new FileStream(@"c:\XmlTest\xmlfile.xml", FileMode.Open)) //
+            {
+                c2 = xmlSerializerCar.Deserialize(file) as Car;
+            } 
+
+            Console.WriteLine(c2);
+        }
+    }
+}
