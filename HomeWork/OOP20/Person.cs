b5d09e618f3e1b6cb7daea3f49ec6a044a3ad1fb
ishaypaz31 <58163486+ishaypaz31@users.noreply.github.com>
2020-03-09 18:37:45 +0200

    IComparer, Casting

---
+++ HomeWork/OOP20/Person.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP20
+{
+    class Person : IComparable<Person>
+    {
+        public int ID { get; private set; }
+
+        public int Age { get; private set; } 
+
+        public float Height { get; private set; }
+
+        public string Name { get; private set; }
+
+        private static readonly IComparer<Person> _compareById;
+        private static readonly IComparer<Person> _compareByAge;
+        private static readonly IComparer<Person> _compareByName;
+        private static readonly IComparer<Person> _compareByHeight;
+
+        public static IComparer<Person> IDComparer
+        {
+            get
+            {
+                return _compareById;
+            }
+        }
+
+        public static IComparer<Person> AgeComparer
+        {
+            get
+            {
+                return _compareByAge;
+            }
+        }
+
+        public static IComparer<Person> HeightComparer
+        {
+            get
+            {
+                return _compareByHeight;
+            }
+        }
+
+
+        public static IComparer<Person> NameComparer
+        {
+            get
+            {
+                return _compareByName;
+            }
+        }
+
+        private static IComparer<Person> DefaultComparer;
+        public static void ModifyDefaultComparer(IComparer<Person> newComparer)
+        {
+            DefaultComparer = newComparer;
+        }
+        private static IComparer<Person> GetDefaultComparer()
+        {
+            return DefaultComparer;
+        }
+
+
+        static Person()
+        {
+
+            DefaultComparer = _compareById; // Etgar metoraf!!!
+        }
+
+        /// <summary>
+        /// Default sort is by ID
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public int CompareTo(Person other)
+        {
+            //---- without Etgar
+            // return this.Id - other.Id;
+
+            // with Etgar metoraf!
+            return DefaultComparer.Compare(this, other);
+        }
+
+        public Person(int iD, int age, float height, string name)
+        {
+            ID = iD;
+            Age = age;
+            Height = height;
+            Name = name;
+        }
+
+        
+        public override string ToString()
+        {
+            return $"ID: {ID,8} , Age: {Age,14} , Height: {Height,15} , Name: {Name,12}";
+        }
+
+
+    }
+}
---
+++ HomeWork/OOP20/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons =
+            {
+                new Person(1, 30, 1.89f, "Oren hazan"),
+                new Person(5, 25, 1.65f, "Tzipi livni"),
+                new Person(4, 22, 1.55f, "Donald Trumph"),
+                new Person(3, 42, 2.05f, "Mickey mouse"),
+                new Person(2, 12, 0.44f, "Goofy")
+            };
+
+            PrintPersons(persons);
+
+
+            // this is with static
+            SortAndPrint(persons, Person.IDComparer);
+            SortAndPrint(persons, Person.AgeComparer);
+            SortAndPrint(persons, Person.HeightComparer);
+            SortAndPrint(persons, Person.NameComparer);
+
+            // this is etgar metoraf!
+            // here we change the default comparer and make the Array.Sort(persons) behave differentlly
+            Array.Sort(persons);
+            PrintPersons(persons); // the default compare is by id
+            Person.ModifyDefaultComparer(Person.NameComparer);
+            Array.Sort(persons);
+            PrintPersons(persons); // now the default sort is by name! hahaha
+            Person.ModifyDefaultComparer(Person.HeightComparer);
+            Array.Sort(persons);
+            PrintPersons(persons); // now the default sort is by height! hahaha
+        }
+
+        static void SortAndPrint(Person[] persons, IComparer<Person> theComparer)
+        {
+            Console.WriteLine("==================");
+            Console.WriteLine($"== Sort by {theComparer.GetType()}");
+            Console.WriteLine("==================");
+            Array.Sort(persons, theComparer);
+            PrintPersons(persons);
+        }
+
+        static void PrintPersons(Person[] persons)
+        {
+            foreach (Person p in persons)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+    }
+}
