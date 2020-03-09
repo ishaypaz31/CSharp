b429af2127802194f6af78c73ee0621f75d40c69
ishaypaz31 <58163486+ishaypaz31@users.noreply.github.com>
2020-03-09 18:59:45 +0200

    Interface

---
+++ Examples/Interface/Shape.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExample
+{
+    // marker interface
+    interface IOldColde
+    {
+
+    }
+
+    interface IGoodCode
+    {
+
+    }
+
+    interface IAreaCalculator
+    {
+        double GetArea(); // public and abstract
+
+    }
+
+    abstract class Shape
+    {
+        public abstract void do1();
+        //public abstract double GetArea();
+    }
+
+    abstract class Circle : Shape, IOldColde, IGoodCode,IAreaCalculator
+    {
+        abstract public double GetArea();
+    }
+
+    class BigCircle : Circle
+    {
+        public override void do1()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double GetArea()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    //marker interface
+    //interface - contract of abilities
+}
