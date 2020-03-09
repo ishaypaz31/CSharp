5873f6142c447e59d634be93fbba59483d9072bf
ishaypaz31 <58163486+ishaypaz31@users.noreply.github.com>
2020-03-09 18:41:54 +0200

    Parameter Overloading

---
+++ HomeWork/OOP22/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Camp Avivim = new Camp(1000, 350, 1600, 200, 30);
+
+            Camp Afikim = new Camp(500, 200, 900, 3000, 1700);
+
+            Camp avivimAfikim = Avivim + Afikim;
+
+            Console.WriteLine($"new camp: = {avivimAfikim}");
+
+            Console.WriteLine($"Avivim > Afikim {Avivim > Afikim}");
+
+            Console.WriteLine($"Avivim < Afikim {Avivim < Afikim}");
+
+            Console.WriteLine(Avivim == Afikim);
+
+            if (Avivim > Afikim)
+                Console.WriteLine("Avivim is bigger");
+
+
+        }
+    }
+}
