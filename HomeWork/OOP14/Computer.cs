7aceed552b411f4fff2dcd874a6bf0a5c612a6d0
ishaypaz31 <58163486+ishaypaz31@users.noreply.github.com>
2020-03-09 18:27:38 +0200

    Object Oriented Programming

---
+++ HomeWork/OOP14/Computer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP14
+{
+    public class Computer
+    {
+        public string _model;
+
+        public int _price;
+
+        public int _numberOfProcessors;
+
+        public double _screenSize;
+
+        public bool _isTurnOn;
+
+
+        public Computer(string Model,int Price,int NumberOfProcessors ,double ScreenSize)
+        {
+            this._model = Model;
+            this._price = Price;
+            this._numberOfProcessors = NumberOfProcessors;
+            this._screenSize = ScreenSize;
+        }
+
+        public double TellMeThePrice()
+        {
+            return _price;
+        }
+
+        public double TellMeTheScreenSize()
+        {
+            return _screenSize;
+        }
+
+        public void TurnOn()
+        {
+            Console.WriteLine("the computer is turn on...");
+        }
+
+
+        public void TurnOff()
+        {
+            Console.WriteLine("the computer is turn off...");
+        }
+
+        public void AddProcessor()
+        {
+           
+        }
+
+        public override string ToString()
+        {
+            return ($"Model: {_model}, Price: {_price}, Number Of Processors: {_numberOfProcessors} ,ScreenSize: {_screenSize}\n");
+            
+        }
+
+    }
+}
---
+++ HomeWork/OOP14/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            Computer ASUS = new Computer("ASUS", 7500, 9, 17.5);
+            ASUS.TellMeThePrice();
+            ASUS.TellMeTheScreenSize();
+
+
+           
+            //2
+            Computer DELL = new Computer("DELL", 9000, 8, 15.5);
+            DELL.TellMeThePrice();
+            DELL.TellMeTheScreenSize();
+
+            //3
+            Computer Apple = new Computer("Apple", 11000, 10, 16.5);
+            Apple.TellMeThePrice();
+            Apple.TellMeTheScreenSize();
+
+          // Apple.TurnOff(); not working good!!!
+
+            Console.WriteLine(ASUS);
+
+            Console.WriteLine(DELL);
+
+            Console.WriteLine(Apple);
+
+            Console.ReadLine();
+        }
+    }
+}
