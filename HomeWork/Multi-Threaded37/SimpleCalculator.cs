398e6cedb76c6b39feef82d7c4a5ecbaab1719ea
ishaypaz31 <58163486+ishaypaz31@users.noreply.github.com>
2020-04-14 11:13:50 +0300

    Delegate,Invoke,Action ,Func

--- HomeWork/Multi-Threaded37/SimpleCalculator.cs
+++ HomeWork/Multi-Threaded37/SimpleCalculator.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//delegate,Invoke,Action ,Func
 namespace MultiThreaded37
 {
     public class SimpleCalculator
