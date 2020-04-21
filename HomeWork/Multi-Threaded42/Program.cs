ffbf263e079d8cb00e0ec660eeef5c54c4695a59
ishaypaz31 <58163486+ishaypaz31@users.noreply.github.com>
2020-04-21 11:27:58 +0300

    ThreadPool , Queue,Monitor,lock

---
+++ HomeWork/Multi-Threaded42/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Multi_Threaded_42
+{
+    class Program
+    {
+        private static void PrintNumbers()
+        {
+            Stopwatch Timer = new Stopwatch();
+            Timer.Start();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(500);
+            }
+            Timer.Stop();
+            Console.WriteLine("Time Elapsed:" + Timer.ElapsedMilliseconds);
+
+        }
+
+        private static void HelloWorld()
+        {
+            Stopwatch Timer2 = new Stopwatch();
+            Timer2.Start();
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine("Hello world!");
+                Thread.Sleep(250);
+            }
+            Timer2.Stop();
+            Console.WriteLine("Time Elapsed:" + Timer2.ElapsedMilliseconds);
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            ThreadExecutor executor = new ThreadExecutor();
+            
+            for (int i = 0; i < 20; i++)
+            {
+                executor.Add(new Thread(PrintNumbers));
+                executor.Execute();
+
+                executor.Add(new Thread(HelloWorld));
+                executor.Execute();
+
+                executor.Add(new Thread(() => Console.WriteLine("Lambda Expression")));
+                executor.Execute();
+            }
+            Console.WriteLine("Main Has Finished ! ! ! ");
+
+            Thread.Sleep(10 * 1000);
+
+
+        }
+    }
+}
---
+++ HomeWork/Multi-Threaded42/ThreadExecutor.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Multi_Threaded_42
+{
+    class ThreadExecutor
+    {
+        Queue<Thread> ThreadsQueue = new Queue<Thread>();
+        Thread _EverySecondThread;
+
+        public void DoEverySecond()
+        {
+            while (true)
+            {
+                lock (this)
+                {
+                    if (ThreadsQueue.Count > 0)
+                    {
+                        Execute();
+                    }
+                }
+                Thread.Sleep(1000);
+            }
+        }
+
+        public ThreadExecutor()
+        {
+
+            //EverySecondThread = new Thread(DoEverySecond);
+
+            //_EverySecondThread.IsBackground = true; // also a solution
+
+            //_EverySecondThread.Start();
+
+            // Thread pool solution
+            //ThreadPool.QueueUserWorkItem(new WaitCallback((object obj) => { DoEverySecond(); })); 
+
+            // Timer solution
+            System.Timers.Timer timer = new System.Timers.Timer(1000);
+            timer.Elapsed += Timer_Elapsed; // ThreadPool
+            timer.Start();
+        }
+
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            lock (this)
+            {
+                if (ThreadsQueue.Count > 0)
+                {
+                    Execute();
+                }
+            }
+        }
+
+        public void ShutDown()
+        {
+            _EverySecondThread.Abort();
+        }
+
+
+
+        public void Add(Thread thread)
+        {
+            lock (this)
+            {
+                if (thread != null)
+                {
+                    Console.WriteLine($"New Thread Entered The Queue");
+                    ThreadsQueue.Enqueue(thread);
+
+                }
+            }
+        }
+
+        public void Execute()
+        {
+            lock (this)
+            {
+                while (ThreadsQueue.Count > 0)
+                {
+                    Thread CurrentThread = ThreadsQueue.Dequeue();
+                    CurrentThread.Start();
+                }
+            }
+        }
+
+
+    }
+}
