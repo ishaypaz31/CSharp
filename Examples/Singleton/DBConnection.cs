17689b432802f4783ea34f97f8a8ff606f5b6c75
ishaypaz31 <58163486+ishaypaz31@users.noreply.github.com>
2020-04-22 21:39:15 +0300

    Singleton design pattern

---
+++ Examples/Singleton/DBConnection.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class DBConnection
+    {
+        //Singleton
+        private static object key = new object();
+        public List<MyConnection> Connctions 
+        {
+            get; set;
+        }
+
+        //3. if instance not exist when requested -> create it!
+        private static DBConnection _instance;
+
+
+        //1. Cannot create more than 1 instance
+        //2. Allow public access to the 1 instance
+        //4. Thread Safe!
+        public static DBConnection GetInstance()
+        {
+            // double check
+            if (_instance == null)
+            {
+                lock (key)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new DBConnection();
+                    }
+                }
+            }
+
+            return _instance;
+        }
+
+        private DBConnection()
+        {
+            Connctions = new List<MyConnection>();
+            Connctions.Add(new MyConnection());
+            Connctions.Add(new MyConnection());
+            Connctions.Add(new MyConnection());
+            Connctions.Add(new MyConnection());
+            Connctions.Add(new MyConnection());
+
+        }
+    }
+}
---
+++ Examples/Singleton/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DBConnection conn1 = DBConnection.GetInstance();
+            DBConnection conn2 = DBConnection.GetInstance();
+
+
+            //var conn = DBConnection.GetInstance().Connctions;
+
+            if (conn1 != conn2)
+            {
+                Console.WriteLine("not a Singleton!!!");
+            }
+            else
+            {
+                Console.WriteLine("Singleton");
+            }
+
+            //DBConnection conn2 = new DBConnection();
+
+        }
+    }
+}
