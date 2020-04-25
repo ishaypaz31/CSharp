785ccd2d3c0cf1755471cd298b1f16a13dd0ad47
ishaypaz31 <58163486+ishaypaz31@users.noreply.github.com>
2020-04-25 11:13:15 +0300

    Task, Auto/ManualRestEvent,

---
+++ HomeWork/Multi-Threaded-44/Customer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Multi_Threaded44
+{
+    public class Customer
+    {
+        public AutoResetEvent are = new AutoResetEvent(false);
+
+
+        public void ConnectToDB()
+        {
+            Console.WriteLine("Connecting to DataBase......");
+            Thread.Sleep(4000);
+            Console.WriteLine("Connected to DataBase! :)");
+            Console.WriteLine();
+            are.Set();
+        }
+
+        public void CustomerCreate()
+        {
+            are.WaitOne();
+            Console.WriteLine("Create a new Customer....");
+            Thread.Sleep(3000);
+            Console.WriteLine("Customer Created! :)");
+            Console.WriteLine();
+            are.Set();
+        }
+
+        public void DeleteCustomer()
+        {
+            are.WaitOne();
+            Console.WriteLine("Deleting Customer.....");
+            Thread.Sleep(3000);
+            Console.WriteLine("Customer deleted! :) ");
+            Console.WriteLine();
+            are.Set();
+        }
+
+        public void DisconnectFromDB()
+        {
+            are.WaitOne();
+            Console.WriteLine("Disconnecting from DataBase.....");
+            Thread.Sleep(3000);
+            Console.WriteLine("DataBase is detached thank you! :) ");
+            Console.WriteLine();
+            are.Set();
+        }
+    }
+}
---
+++ HomeWork/Multi-Threaded-44/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Multi_Threaded44
+{
+    class Program
+    {
+        public static Random random = new Random();
+
+        static void Main(string[] args)
+        {
+            Task<int> TaskINT1 = new Task<int>(() =>
+            {
+                Console.WriteLine($"Task ID: {Thread.CurrentThread.ManagedThreadId}");
+                int t1 = random.Next(1000, 2000);
+                Thread.Sleep(t1);
+                return t1;
+            }, TaskCreationOptions.LongRunning);
+            Task<int> TaskINT2 = new Task<int>(() =>
+            {
+                Console.WriteLine($"Task ID: {Thread.CurrentThread.ManagedThreadId}");
+                int t2 = random.Next(1000, 2000);
+                Thread.Sleep(t2);
+                return t2;
+            }, TaskCreationOptions.LongRunning);
+
+            TaskINT1.Start();
+            TaskINT2.Start();
+            Task.WaitAny(TaskINT1, TaskINT2);
+            if (TaskINT1.IsCompleted)
+            {
+                Console.WriteLine($"Task result is {TaskINT1.Result}");
+                Console.WriteLine($"Task result is {TaskINT2.Result}");
+            }
+            else
+            {
+                Console.WriteLine($"Task result is {TaskINT1.Result}");
+                Console.WriteLine($"Task result is {TaskINT2.Result}");
+            }
+
+            Console.WriteLine("---------------------------");
+
+            Customer cust = new Customer();
+            new Thread(cust.ConnectToDB).Start();
+            new Thread(cust.CustomerCreate).Start();
+            new Thread(cust.DeleteCustomer).Start();
+            new Thread(cust.DisconnectFromDB).Start();
+
+
+
+        }
+    
+    }
+}
