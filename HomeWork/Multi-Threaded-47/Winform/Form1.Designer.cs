40cebb36538149baf3c5ad2a4390cb7a6d58aff3
ishaypaz31 <58163486+ishaypaz31@users.noreply.github.com>
2020-04-27 09:56:06 +0300

    WinformTimer

---
+++ HomeWork/Multi-Threaded-47/Winform/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.displayTimeBtn = new System.Windows.Forms.Button();
+            this.timeLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // displayTimeBtn
+            // 
+            this.displayTimeBtn.Location = new System.Drawing.Point(55, 68);
+            this.displayTimeBtn.Name = "displayTimeBtn";
+            this.displayTimeBtn.Size = new System.Drawing.Size(141, 86);
+            this.displayTimeBtn.TabIndex = 0;
+            this.displayTimeBtn.Text = "Click for timer";
+            this.displayTimeBtn.UseVisualStyleBackColor = true;
+            this.displayTimeBtn.Click += new System.EventHandler(this.displayTimeBtn_Click);
+            // 
+            // timeLbl
+            // 
+            this.timeLbl.AutoSize = true;
+            this.timeLbl.Location = new System.Drawing.Point(63, 204);
+            this.timeLbl.Name = "timeLbl";
+            this.timeLbl.Size = new System.Drawing.Size(107, 20);
+            this.timeLbl.TabIndex = 1;
+            this.timeLbl.Text = "I\'m Waiting.....";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 328);
+            this.Controls.Add(this.timeLbl);
+            this.Controls.Add(this.displayTimeBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button displayTimeBtn;
+        private System.Windows.Forms.Label timeLbl;
+
+    }
+}
+
---
+++ HomeWork/Multi-Threaded-47/Winform/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        System.Timers.Timer timer = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void DrawEvery100()
+        {
+            while (true)
+            {
+                timeLbl.Text = DateTime.Now.Millisecond.ToString();
+                timeLbl.ForeColor = DateTime.Now.Millisecond % 2 == 0 ? Color.Green : Color.Red;
+                await Task.Run(() => { Thread.Sleep(100); });
+            }
+
+        }
+
+        private void ElapsedEventHandler(object sender, ElapsedEventArgs e)
+        {
+            Action action = () => {
+                timeLbl.Text = DateTime.Now.Millisecond.ToString();
+                timeLbl.ForeColor = DateTime.Now.Millisecond % 2 == 0 ? Color.Green : Color.Red;
+            };
+            timeLbl.BeginInvoke(action);
+        }
+        private void displayTimeBtn_Click(object sender, EventArgs e)
+        {
+            // Option 1
+            
+            if (timer == null)
+            {
+                timer = new System.Timers.Timer(100);
+                timer.Elapsed += ElapsedEventHandler;
+                timer.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Timer already on!");
+            }
+            
+            DrawEvery100();
+        }
+    }
+}
