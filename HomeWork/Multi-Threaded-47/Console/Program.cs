13815f5c2316dd9f3a70619a405e6ee1344b35a8
ishaypaz31 <58163486+ishaypaz31@users.noreply.github.com>
2020-04-27 09:55:01 +0300

    Async, FileWriter

---
+++ HomeWork/Multi-Threaded-47/Console/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Multi_Threaded_47
+{
+    class Program
+    {
+        public const string FilePath = @"C:\sqllite\Text.txt";
+        public const int Range = 1000;
+        public const int Delay = 100;
+
+        private async static Task WriteToFile()
+        {
+            StreamWriter sw = new StreamWriter(FilePath);
+            for (int i = 1; i <= Range; i++)
+            {
+                await sw.WriteAsync(i.ToString() + " ");
+                Thread.Sleep(10);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ready. Wait 5 seconds ... very busy now");
+            Task t = WriteToFile();
+            while (!t.Wait(10))
+            {
+                Console.Write(".");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Done!!!");
+        }
+        
+    }
+}
+
+
